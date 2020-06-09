5af19b505136add73a2c39fac72844b1aa51f397
leechongyan <leechongyan33@gmail.com>
2020-06-09 23:17:29 +0800

    set up basic routing

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module github.com/leechongyan/go_chat_app
 
 go 1.14
+
+require (
+	github.com/cenkalti/backoff v2.2.1+incompatible // indirect
+	github.com/dancannon/gorethink v4.0.0+incompatible
+	github.com/gorilla/websocket v1.4.2
+	gopkg.in/dancannon/gorethink.v0 v0.7.2 // indirect
+	gopkg.in/gorethink/gorethink.v4 v4.1.0 // indirect
+	gopkg.in/rethinkdb/rethinkdb-go.v6 v6.2.1
+)
---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	r "github.com/dancannon/gorethink"
+)
+
+func main() {
+	// establish rethinkdb address
+	session, err := r.Connect(r.ConnectOpts{
+		Address:  "localhost:28015",
+		Database: "chat_database",
+	})
+
+	if err != nil {
+		log.Panic(err.Error())
+	}
+
+	router := NewRouter(session)
+
+	// TODO: handles
+	router.Handle("channel add", addChannel)
+	router.Handle("channel subscribe", subscribeChannel)
+	router.Handle("channel unsubscribe", unsubscribeChannel)
+
+	router.Handle("user edit", editUser)
+	router.Handle("user subscribe", subscribeUser)
+	router.Handle("user unsubscribe", unsubscribeUser)
+
+	router.Handle("message add", addChannelMessage)
+	router.Handle("message subscribe", subscribeChannelMessage)
+	router.Handle("message unsubscribe", unsubscribeChannelMessage)
+
+	http.Handle("/", router)
+	http.ListenAndServe(":4000", nil)
+}
